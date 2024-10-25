54065194ae1981b34af966c32fc3bf78a2df4ab1
Angel Cancho <u201721995@upc.edu.pe>
2024-10-25 02:38:29 -0500

    feat: enhance CharacterModel to include status, species, and gender fields

--- lib/features/character/data/remote/character_model.dart
+++ lib/features/character/data/remote/character_model.dart
@@ -1,15 +1,27 @@
 class CharacterModel {
-  int id;
-  String name;
-  String image;
+  final int id;
+  final String name;
+  final String image;
+  final String status;
+  final String species;
+  final String gender;
 
-  CharacterModel({required this.id, required this.name, required this.image});
+  CharacterModel(
+      {required this.id,
+      required this.name,
+      required this.image,
+      required this.status,
+      required this.species,
+      required this.gender});
 
   factory CharacterModel.fromJson(Map<String, dynamic> json) {
     return CharacterModel(
-      id: json['id'] ?? '',
+      id: json['id'] ?? 0,
       name: json['name'] ?? '',
       image: json['image'] ?? '',
+      status: json['status'] ?? '',
+      species: json['species'] ?? '',
+      gender: json['gender'] ?? '',
     );
   }
 }
